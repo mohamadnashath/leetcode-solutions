7ed072efaaca75ab0b854bafc10c26ed9617d03c
mohamadnashath <mohamadnashath2007@gmail.com>
2026-08-03 18:51:29 +0530

    solve #496 next greater element i

---
+++ 0496_next_greater_element_i.py
@@ -0,0 +1,14 @@
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        n1idx={n:i for i,n in enumerate(nums1)}
+        res=[-1]*len(nums1)
+        stack=[]
+        for i in range(len(nums2)):
+            cur=nums2[i]
+            while stack and cur>stack[-1]:
+                val=stack.pop()
+                idx=n1idx[val]
+                res[idx]=cur
+            if cur in n1idx:
+                stack.append(cur)
+        return res
