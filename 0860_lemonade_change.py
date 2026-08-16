1d37a14bf074bd69063062097c6e5c52f5ddb956
mohamadnashath <mohamadnashath2007@gmail.com>
2026-08-16 19:36:35 +0530

    solve #860 lemonade change

---
+++ 0860_lemonade_change.py
@@ -0,0 +1,23 @@
+class Solution:
+    def lemonadeChange(self, bills: List[int]) -> bool:
+        count5=0
+        count10=0
+        for i in range (len(bills)):
+            if bills[i]==5:
+                count5+=1
+            elif bills[i]==10:
+                if count5>0:
+                    count5-=1
+                    count10+=1
+                else:
+                    return False
+            else:
+                if count10>0 and count5>0:
+                    count10-=1
+                    count5-=1
+                elif count5>2:
+                    count5-=3
+                else:
+                    return False
+        return True
+        
