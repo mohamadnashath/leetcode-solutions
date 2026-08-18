5f5b367f3b42cdde9b37eefc58b4f53da7bc93b9
mohamadnashath <mohamadnashath2007@gmail.com>
2026-08-18 22:04:45 +0530

    solve #45 jump game ii

---
+++ 0045_jump_game_ii.py
@@ -0,0 +1,12 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        jump,left,right=0,0,0
+        n=len(nums)
+        while right<n-1:
+            farthest=0
+            for i in range(left,right+1):
+                farthest=max(farthest,i+nums[i])
+            left=right+1
+            right=farthest
+            jump+=1
+        return jump
